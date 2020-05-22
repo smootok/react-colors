a6875dab4738e7e6bb6b6725f3b889f68b6d8428
smootok <smootok@gmail.com>
2020-05-22 23:26:53 +0000

    Add copy color using react-copy-to-clipboard module and add some global styles

--- src/color-box.js
+++ src/color-box.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 import './color-box.css'
 
 export default function ColorBox ({ name, background }) {
   return (
-    <div style={{ background }} className='color-box'>
-      <div className='copy-container'>
-        <div className='box-content'>
-          <span>{name}</span>
+    <CopyToClipboard text={background}>
+      <div style={{ background }} className='color-box'>
+        <div className='copy-container'>
+          <div className='box-content'>
+            <span>{name}</span>
+          </div>
+          <button className='copy-button'>Copy</button>
         </div>
-        <button className='copy-button'>Copy</button>
+        <span className='see-more'>More</span>
       </div>
-      <span className='see-more'>More</span>
-    </div>
+    </CopyToClipboard>
   )
 }
