c57e183a04e7e2a8a49de0f94756da568b93a2e6
smootok <smootok@gmail.com>
2020-05-23 06:50:35 +0000

    Add palette helper to generate new palette

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 import Palette from './palette'
 import palettesConfig from './palettes.config'
+import { generatePalette } from './palettes.helpers'
+
+console.log(generatePalette(palettesConfig[4]))
 
 export default function App () {
   return (
---
+++ src/palettes.helpers.js
@@ -0,0 +1,42 @@
+import chroma from 'chroma-js'
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900]
+
+const getRange = hexColor => {
+  const end = '#fff'
+  return [
+    chroma(hexColor)
+      .darken(1.4)
+      .hex(),
+    hexColor,
+    end
+  ]
+}
+
+const getScale = (hexColor, numberOfColors) => {
+  return chroma
+    .scale(getRange(hexColor))
+    .mode('lab')
+    .colors(numberOfColors)
+}
+
+export const generatePalette = starterPalette => {
+  const newPalette = { ...starterPalette, colors: {} }
+
+  for (const level of levels) {
+    newPalette.colors[level] = []
+  }
+
+  for (const color of starterPalette.colors) {
+    const scale = getScale(color.color, 10).reverse()
+    for (const i in scale) {
+      newPalette.colors[levels[i]].push({
+        id: color.name.toLowerCase().replace(/ /g, '-'),
+        name: `${color.name} ${levels[i]}`,
+        hex: scale[i],
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i]).css().replace('rgb', 'rgba').replace(')', ',1.0)')
+      })
+    }
+  }
+  return newPalette
+}
