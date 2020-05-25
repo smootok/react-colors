792de2292db05bacbbd2b320d45a6b43f8e83010
smootok <smootok@gmail.com>
2020-05-25 18:38:25 +0000

    Add snackbar and style select

--- src/navbar.js
+++ src/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Select from '@material-ui/core/Select'
-import { MenuItem } from '@material-ui/core'
+import { MenuItem, IconButton, Snackbar } from '@material-ui/core'
+import { Close as CloseIcon } from '@material-ui/icons'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 
@@ -12,6 +13,13 @@ export default function Navbar ({
   colorFormat,
   setColorFormat
 }) {
+  const [open, setOpen] = React.useState(false)
+
+  const handleColorFormatChange = e => {
+    setColorFormat(e.target.value)
+    setOpen(true)
+  }
+
   return (
     <header className='navbar'>
       <div className='logo'>
@@ -32,13 +40,42 @@ export default function Navbar ({
       <div className='select-container'>
         <Select
           value={colorFormat}
-          onChange={e => setColorFormat(e.target.value)}
+          onChange={handleColorFormatChange}
         >
           <MenuItem value='hex'>HEX - #ffffff</MenuItem>
           <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
           <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        open={open}
+        autoHideDuration={3000}
+        onClose={() => setOpen(false)}
+        message={
+          <span id='message-id'>
+            Format changed to {colorFormat.toUpperCase()}!
+          </span>
+        }
+        ContentProps={{
+          'aria-describedby': 'message-id'
+        }}
+        action={
+          <>
+            <IconButton
+              size='small'
+              aria-label='close'
+              color='inherit'
+              onClick={() => setOpen(false)}
+            >
+              <CloseIcon fontSize='small' />
+            </IconButton>
+          </>
+        }
+      />
     </header>
   )
 }
