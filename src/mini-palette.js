57f091daf0aeebc35aadcf3ae70dcfd599ca4b52
smootok <smootok@gmail.com>
2020-05-29 22:11:42 +0000

    Add mini-palette and style palette-list and mini-palette using material ui

---
+++ src/mini-palette.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#fff',
+    border: '1px solid #000',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer'
+    }
+  },
+  colors: {
+    backgroundColor: '#dae1e4',
+    height: '150px',
+    width: '100%',
+    borderRadius: '5px',
+    overflow: 'hidden'
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: '#000',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
+  },
+  emoji: {
+    marginLeft: '0.5rem',
+    fontSize: '1.5rem'
+  },
+  miniColor: {
+    height: '25%',
+    width: '20%',
+    display: 'inline-block',
+    margin: '0 auto',
+    position: 'relative',
+    marginBottom: '-3.5px'
+  }
+})
+
+export default function MiniPalette ({ paletteName, emoji, colors }) {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.colors}>
+        {colors.map(color => (
+          <div
+            className={classes.miniColor}
+            style={{ backgroundColor: color.color }}
+            key={color.name}
+          />
+        ))}
+      </div>
+      <h5 className={classes.title}>
+        {paletteName}
+        <span className={classes.emoji}>{emoji}</span>
+      </h5>
+    </div>
+  )
+}
--- src/palette-list.js
+++ src/palette-list.js
@@ -1,15 +1,51 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/styles'
+
+import MiniPalette from './mini-palette'
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'blue',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  container: {
+    width: '50%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    flexWrap: 'wrap'
+  },
+  nav: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between'
+  },
+  palettes: {
+    boxSizing: 'border-box',
+    width: '100%',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 30%)',
+    gridGap: '5%'
+  }
+})
 
 export default function PaletteList ({ palettes }) {
+  const classes = useStyles()
   return (
-    <div>
-      <h1>React Colors</h1>
-      {palettes.map(palette => (
-        <div key={palette.id}>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map(palette => (
+            <MiniPalette key={palette.id} {...palette} />
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   )
 }
