e0a288a0e85a63d567347d3514ebc7b584f45f1a
smootok <smootok@gmail.com>
2020-06-02 17:46:58 +0100

    Refactor color-box css and more-palette css

--- src/color-box.js
+++ src/color-box.js
@@ -1,9 +1,127 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/styles'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import chroma from 'chroma-js'
 
-import './color-box.css'
+const useStyles = makeStyles({
+  root: {
+    width: '20%',
+    height: props => (props.showLink ? '25%' : '50%'),
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-3.5px',
+    '&:hover button': {
+      opacity: 1
+    }
+  },
+  copyText: {
+    color: props =>
+      chroma(props.background).luminance() >= 0.7 ? 'black' : 'white'
+  },
+  colorName: {
+    color: props =>
+      chroma(props.background).luminance() <= 0.08 ? 'white' : 'black'
+  },
+  seeMore: {
+    color: props =>
+      chroma(props.background).luminance() >= 0.7 ? 'rgba(0,0,0,0.6)' : 'white',
+    background: 'rgba(255, 255, 255, 0.3)',
+    position: 'absolute',
+    border: 'none',
+    right: '0px',
+    bottom: '0px',
+    width: '60px',
+    height: '30px',
+    textAlign: 'center',
+    lineHeight: '30px',
+    textTransform: 'uppercase'
+  },
+  copyButton: {
+    color: props =>
+      chroma(props.background).luminance() >= 0.7 ? 'rgba(0,0,0,0.6)' : 'white',
+    width: '100px',
+    height: '30px',
+    position: 'absolute',
+    display: 'inline-block',
+    top: '50%',
+    left: '50%',
+    marginLeft: '-50px',
+    marginTop: '-15px',
+    textAlign: 'center',
+    outline: 'none',
+    background: 'rgba(255, 255, 255, 0.3)',
+    fontSize: '1rem',
+    lineHeight: '30px',
+    textTransform: 'uppercase',
+    border: 'none',
+    textDecoration: 'none',
+    opacity: 0
+  },
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    padding: '10px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px'
+  },
+  copyOverlay: {
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)'
+  },
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+    position: 'absolute'
+  },
+  copyMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    fontSize: '4rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: 'white',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.2)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase'
+    },
+    '& p': {
+      fontSize: '2rem',
+      fontWeight: '100'
+    }
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.3s'
+  }
+})
 
 export default function ColorBox ({
   id,
@@ -13,9 +131,7 @@ export default function ColorBox ({
   showLink
 }) {
   const [copied, setCopied] = React.useState(false)
-
-  const isDarkColor = chroma(background).luminance() <= 0.08
-  const isLightColor = chroma(background).luminance() >= 0.7
+  const classes = useStyles({ background, showLink })
 
   React.useEffect(() => {
     if (!copied) return
@@ -26,31 +142,29 @@ export default function ColorBox ({
 
   return (
     <CopyToClipboard text={background} onCopy={() => setCopied(true)}>
-      <div style={{ background }} className='color-box'>
+      <div style={{ background }} className={classes.root}>
         <div
           style={{ background }}
-          className={`copy-overlay ${copied ? 'show' : ''}`}
+          className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
         />
-        <div className={`copy-msg ${copied ? 'show' : ''}`}>
-          <h1>Copied!</h1>
-          <p className={isLightColor && 'dark-text'}>{background}</p>
+        <div
+          className={`${classes.copyMessage} ${copied && classes.showMessage}`}
+        >
+          <h1>copied!</h1>
+          <p className={classes.copyText}>{background}</p>
         </div>
-        <div className='copy-container'>
-          <div className='box-content'>
-            <span className={isDarkColor && 'light-text'}>{name}</span>
+        <div>
+          <div className={classes.boxContent}>
+            <span className={classes.colorName}>{name}</span>
           </div>
-          <button className={`copy-button ${isLightColor && 'dark-text'}`}>
-            Copy
-          </button>
+          <button className={classes.copyButton}>Copy</button>
         </div>
         {showLink && (
           <Link
             to={`/palette/${paletteId}/${id}`}
             onClick={e => e.stopPropagation()}
           >
-            <span className={`see-more ${isLightColor && 'dark-text'}`}>
-              More
-            </span>
+            <span className={classes.seeMore}>More</span>
           </Link>
         )}
       </div>
--- src/more-palette.js
+++ src/more-palette.js
@@ -1,12 +1,55 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/styles'
 
 import ColorBox from './color-box'
 import PaletteFooter from './palette-footer'
 import Navbar from './navbar'
 
+const useStyles = makeStyles({
+  root: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  colors: {
+    height: '90%'
+  },
+  goBack: {
+    width: '20%',
+    height: '50%',
+    margin: '0 auto',
+    display: 'inline-block',
+    position: 'relative',
+    cursor: 'pointer',
+    marginBottom: '-3.5px',
+    opacity: 1,
+    backgroundColor: '#000',
+    '& a': {
+      color: '#fff',
+      width: '100px',
+      height: '30px',
+      position: 'absolute',
+      display: 'inline-block',
+      top: '50%',
+      left: '50%',
+      marginLeft: '-50px',
+      marginTop: '-15px',
+      textAlign: 'center',
+      outline: 'none',
+      background: 'rgba(255, 255, 255, 0.3)',
+      fontSize: '1rem',
+      lineHeight: '30px',
+      textTransform: 'uppercase',
+      border: 'none',
+      textDecoration: 'none'
+    }
+  }
+})
+
 export default function MorePalette ({ palette, colorId }) {
   const [colorFormat, setColorFormat] = React.useState('hex')
+  const classes = useStyles()
 
   const gatherShades = (palette, colorId) => {
     let shades = []
@@ -22,13 +65,13 @@ export default function MorePalette ({ palette, colorId }) {
   const shades = gatherShades(palette, colorId)
 
   return (
-    <div className='more-palette palette'>
+    <div className={classes.root}>
       <Navbar
         colorFormat={colorFormat}
         setColorFormat={setColorFormat}
         showingAllColors={false}
       />
-      <div className='palette-colors'>
+      <div className={classes.colors}>
         {shades.map(color => (
           <ColorBox
             key={color.name}
@@ -38,11 +81,9 @@ export default function MorePalette ({ palette, colorId }) {
             showLink={false}
           />
         ))}
-        <Link to={`/palette/${palette.id}`}>
-          <div className='color-box' style={{ backgroundColor: '#000' }}>
-            <span className='back-button'>Go back</span>
-          </div>
-        </Link>
+        <div className={classes.goBack}>
+          <Link to={`/palette/${palette.id}`}>Go Back</Link>
+        </div>
         <PaletteFooter
           paletteName={palette.paletteName}
           emoji={palette.emoji}
